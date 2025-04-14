34171cd0f30054339926f45483b98528f26ad20c
Ba Dac <lebadac@Bas-MacBook-Pro.local>
2025-04-14 20:48:12 +0700

    Add comments

--- server.py
+++ server.py
@@ -4,36 +4,30 @@ import torch
 
 app = Flask(__name__)
 
-# Load PhoBERT model
+# Load the pre-trained PhoBERT sentiment classification model and tokenizer
 model = RobertaForSequenceClassification.from_pretrained("wonrax/phobert-base-vietnamese-sentiment")
 tokenizer = AutoTokenizer.from_pretrained("wonrax/phobert-base-vietnamese-sentiment", use_fast=False)
 
+# Sentiment labels corresponding to the model's output classes
 labels = ['negative', 'positive', 'neutral']
 
-
+# Endpoint to analyze Vietnamese text sentiment using a pre-trained PhoBERT model
 @app.route('/analyze', methods=['POST'])
 def analyze_sentiment():
     data = request.get_json()
     text = data.get('text', '')
-
     inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
     with torch.no_grad():
         outputs = model(**inputs)
         probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
-
-        # # In ra các xác suất để kiểm tra (trước khi lấy nhãn)
         # print(f"Probabilities: {probs.tolist()}")
-
-        # Đảm bảo lấy đúng thứ tự: [NEG, POS, NEU]
         pred = torch.argmax(probs, dim=-1).item()
-
-        # Trả lại kết quả với xác suất từng lớp
         label = labels[pred]
         return jsonify({'label': label})
 
 
 
-
+#Run app
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
 
